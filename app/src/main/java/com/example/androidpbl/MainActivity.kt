e33b495a4e473d74769162c1a8d0c3acad0ed780
dongjun-Yi <djyi0223@gmail.com>
2022-11-06 13:25:49 +0900

    회원가입 UI

--- app/src/main/java/com/example/androidpbl/MainActivity.kt
+++ app/src/main/java/com/example/androidpbl/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.androidpbl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        val signupButton = findViewById<Button>(R.id.signupButton)
+        signupButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
