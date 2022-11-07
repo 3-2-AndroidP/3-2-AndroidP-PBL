d5e9d8449b945c7ab379106d7c8b1d3502f8fb48
kys0417 <yungsang1111@gmail.com>
2022-11-07 23:59:19 +0900

    postadd, frienddetail xml 추가

--- app/src/main/java/com/example/androidpbl/MainActivity.kt
+++ app/src/main/java/com/example/androidpbl/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.androidpbl
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +19,11 @@ class MainActivity : AppCompatActivity() {
         article2.setBackgroundColor(Color.GRAY)
         val article3 = findViewById<Button>(R.id.articleButton3)
         article3.setBackgroundColor(Color.GRAY)
+
+        val postAddButton = findViewById<ImageButton>(R.id.postAddButton)
+        postAddButton.setOnClickListener {
+            val intent = Intent(this, PostAddActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
